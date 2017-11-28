6c5cfd3d3c10e6b75d88a9a130e9437491668d80
vil17 <34066610+vil17@users.noreply.github.com>
2017-11-28 19:18:32 +0000

    virkar
    tested

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ofstream fout;
+    string typethis = "";
+
+    cout << "Type \\ to quit" << endl;
+    do {
+            fout.open("streamfile.txt", ios::app);
+        if (fout.is_open()) {
+            cout << "This will be added:  ";
+            getline(cin, typethis);
+            if(typethis[0] == '\\'){
+                break;
+            }
+            else{
+            }
+            fout << " " << typethis << endl;
+        }                else {
+            cout << "file not open";
+        }
+        fout.close();
+    } while(typethis[0] != '\\');
+
+    fout.close();
+
     return 0;
 }
